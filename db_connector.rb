b92a8cf1722bdfb8fb6668f6e79e89729d3cee7d
Vlad <vlad.golovchak.1996@gmail.com>
2021-10-27 15:32:29 +0300

    refactor code, create db_connector

--- db_connector.rb
+++ db_connector.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
 class DbConnector
+  attr_reader :array_from_db
 
+  def initialize
+    @client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'candidate_test')
+    @array_from_db = []
+  end
+
+  def form_array_from_db_select
+    results = @client.query('SELECT candidate_office_name FROM hle_dev_test_vholovchak')
+    results.each do |row|
+      @array_from_db << row['candidate_office_name']
+    end
+  end
+
+  def update_table(corrected_array)
+    corrected_array.each_with_index do |element, index|
+      sentence = "The candidate is running for the #{element} office."
+      @client.query("UPDATE hle_dev_test_vholovchak SET clean_name ='#{element}', sentence = '#{sentence}' WHERE id = '#{index + 1}'")
+    end
+    @client.close
+  end
 end
--- main.rb
+++ main.rb
@@ -4,32 +4,9 @@ require_relative 'candidate_formatter'
 require_relative 'db_connector'
 require 'mysql2'
 
-DbConnector.new
+db_connect = DbConnector.new
+db_connect.form_array_from_db_select
 
-array_from_db = []
+corrected_array = CandidateFormatter.new(db_connect.array_from_db).call
 
-client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'candidate_test')
-results = client.query('SELECT candidate_office_name FROM hle_dev_test_vholovchak')
-results.each do |row|
-  array_from_db << row['candidate_office_name']
-end
-
-# test = ['Something Township, Lol Like',
-#         'County Clerk/Recorder/DeKalb County',
-#         'Highway Highway Clerk/Recorder/DeKalb County',
-#         '1st circuit judge, Circuit Court',
-#         'County Clerk/Recorder/DeKalb County',
-#         'State Central Committeeman/12th District',
-#         'Township Committeeman/Palos Twp',
-#         'Judge, Circuit Court/15th Circuit'
-#        ]
-
-corrected_array = CandidateFormatter.new(array_from_db).call
-
-corrected_array.each_with_index do |element, index|
-  sentence = "The candidate is running for the #{element} office."
-  client.query("UPDATE hle_dev_test_vholovchak SET clean_name ='#{element}', sentence = '#{sentence}' WHERE id = '#{index + 1}'")
-end
-
-client.close
-
+db_connect.update_table(corrected_array)
